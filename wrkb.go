69b8975664ace7ccdb94f0e2740d5fd0d797d325
yykhomenko <yykhomenko@gmail.com>
2020-12-30 13:06:46 +0200

    able to run wrk command

--- wrkb.go
+++ wrkb.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os/exec"
+	"strings"
+)
 
+func main() {
+	p := strings.Split("wrk -t1 -c1 -d1s --latency http://127.0.0.1", " ")
+	cmd := exec.Command(p[0], p[1:]...)
+	b, err := cmd.Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(b))
 }
