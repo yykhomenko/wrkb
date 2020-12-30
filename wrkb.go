7c4285392ad9d4d56dc762bb68e2fc35cf0d7605
yykhomenko <yykhomenko@gmail.com>
2020-12-30 15:26:52 +0200

    num conns as config

--- wrkb.go
+++ wrkb.go
@@ -8,19 +8,27 @@ import (
 )
 
 func main() {
-	arg := strings.Split("wrk -t1 -c1 -d1s --latency http://127.0.0.1", " ")
-	cmd := exec.Command(arg[0], arg[1:]...)
+	load(1)
+	load(10)
+}
+
+func load(c int) {
+	args := strings.Split(command(c), " ")
+	cmd := exec.Command(args[0], args[1:]...)
 	b, err := cmd.Output()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(string(b))
+	// fmt.Println(string(b))
 
 	p := split(string(b))
-	fmt.Println()
-	fmt.Println(p)
-	fmt.Println(p[11][1])
-	fmt.Println(p[3][1])
+	// fmt.Println()
+	// fmt.Println(p)
+	fmt.Println(p[11][1], p[3][1])
+}
+
+func command(c int) string {
+	return fmt.Sprintf("wrk -t1 -c%d -d1s --latency http://127.0.0.1", c)
 }
 
 func split(in string) (out [][]string) {
